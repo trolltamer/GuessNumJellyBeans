fa45ca335336a13fa0db899734cbc9d204353ce6
Paul Younger <pyounger22@gmail.com>
2018-08-29 13:22:38 -0700

    Update GuessNumJellyBeans.cs

--- GuessNumJellyBeans.cs
+++ GuessNumJellyBeans.cs
@@ -3,20 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-
 /*GuessNumJellyBeans takes an array of ints representing guesses at the number of jelly beans contained in a jar, then based solely on the list
  * of guesses the function returns its best guess as to the number of jelly beans contained in the jar. If there are 2 or more guesses then the function
  * should return the average of all the guesses. If there is only one guess the function should randomly guess either one higher or one lower than
  * the guess. GuessArray has a minimum of one member and can contain infinitely many guesses. 
  */
-
 class Program
 {
-    static void Main(string[] args)
-
+    static void Main(string[] args
     {
-
         int[] test0 = new int[] {15}; //should return 14 or 16, can be run multiple times to show variation
         Console.WriteLine(GuessNumJellyBeans(test0));
 
